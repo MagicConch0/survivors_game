25dcad3085d9277aea787f43fa29a9dec234b236
MagicConch0 <867786975@qq.com>
2025-04-03 15:42:39 +0800

    修复某些bug

--- scenes/UI/AbilityUpgradeCard.cs
+++ scenes/UI/AbilityUpgradeCard.cs
@@ -3,9 +3,10 @@ using System;
 /* UI界面：生成升级卡片，用于描述升级信息（技能名字，作用等） */
 public partial class AbilityUpgradeCard : PanelContainer
 {
-/* 设置升级卡片上显示的信息 */
-  public void SetAbilityUpgrade(Ability_upgrade ability_Upgrade){
-	GetNode<Label>("%NameLabel").Text = ability_Upgrade.name;
-	GetNode<Label>("%DescriptionLabel").Text = ability_Upgrade.description;
+  /* 设置升级卡片上显示的信息 */
+  public void SetAbilityUpgrade(Ability_upgrade ability_Upgrade)
+  {
+    GetNode<Label>("%NameLabel").Text = ability_Upgrade.name;
+    GetNode<Label>("%DescriptionLabel").Text = ability_Upgrade.description;
   }
 }
